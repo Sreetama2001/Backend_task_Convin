adff628c9bb573b1000db3c7e6fcf3e56320b25d
Sreetama <fool100abc@gmail.com>
2023-05-27 00:50:44 +0530

    Project files added

---
+++ googleCalender_backendTask/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from views import CalenderInitView, CalenderRedirectView, home
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('rest/v1/calendar/init/', CalenderInitView, name='google-calendar-init'),
+    path('rest/v1/calendar/redirect/', CalenderRedirectView, name='google-calendar-redirect'),
+]
+
